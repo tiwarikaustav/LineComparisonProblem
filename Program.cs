551e84a999275fb6fdb5076280e4f6c1bf8a123d
Aryav <tiwarikaustav@gmail.com>
2020-09-30 21:34:15 +0530

    Length of Line Segment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace LineComparisonProblem
 {
@@ -6,7 +7,24 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Problem!");
+            int x1 = RandNum(), y1 = RandNum();
+            int x2 = RandNum(), y2 = RandNum();
+            Console.WriteLine("Coordinates of End Point One: ({0}, {0})", x1, y1);
+            Console.WriteLine("Coordinates of End Point Two: ({0}, {0})", x2, y2);
+            Console.WriteLine("Length of Line Segment is: {0}", DistanceFormula2D(x1, y1, x2, y2));
+        }
+
+        // Calculates distance between 2 points in 2-d cartesian plane
+        static double DistanceFormula2D(int x1, int y1, int x2, int y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        }
+
+        //Generates random number between 1 and 99
+        static int RandNum()
+        {
+            Random randObj = new Random();
+            return randObj.Next(1, 100);
         }
     }
 }
